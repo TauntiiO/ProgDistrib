4475b6f015b92bee020dd3281ce84bff53b009c6
TauntiO <titouan.br64@gmail.com>
2025-03-26 09:11:04 -0400

    fix: car rental

--- TP1REST/TP1REST/src/main/java/com/example/TP1REST/CarRentalController.java
+++ TP1REST/TP1REST/src/main/java/com/example/TP1REST/CarRentalController.java
@@ -1,7 +1,6 @@
 package com.example.TP1REST;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 public class CarRentalController {
